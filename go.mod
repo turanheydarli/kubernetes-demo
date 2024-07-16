ceca684c1e06c750639b9d1c5c772581db43b1a5
Turan Heydarli <turan.heydarli@code.edu.az>
2024-07-16 10:48:16 +0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module kubernetesdemo
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+)
+
+var build = "development"
+
+func main() {
+
+	g := runtime.GOMAXPROCS(0)
+
+	log.Printf("starting service build[%s] CPU[%d]", build, g)
+	defer log.Println("service ended")
+
+	shutdown := make(chan os.Signal, 1)
+
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	log.Println("stopping service")
+}
